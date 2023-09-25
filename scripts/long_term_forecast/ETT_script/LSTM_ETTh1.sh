71b7d91ca684013912e2ff740c67d4f92c5742ca
GingerHannibal <sydinthjz@outlook.com>
2023-09-25 12:15:53 +0800

    0925

--- scripts/long_term_forecast/ETT_script/LSTM_ETTh1.sh
+++ scripts/long_term_forecast/ETT_script/LSTM_ETTh1.sh
@@ -2,40 +2,18 @@ export CUDA_VISIBLE_DEVICES=0
 
 model_name=LSTM
 
-# python -u run.py \
-#   --task_name long_term_forecast \
-#   --is_training 1 \
-#   --root_path ./dataset/ETT-small/ \
-#   --data_path ETTh1.csv \
-#   --model_id ETTh1_96_96 \
-#   --model $model_name \
-#   --data ETTh1 \
-#   --features M \
-#   --seq_len 96 \
-#   --label_len 48 \
-#   --pred_len 96 \
-#   --e_layers 2 \
-#   --d_layers 1 \
-#   --factor 3 \
-#   --enc_in 7 \
-#   --dec_in 7 \
-#   --c_out 7 \
-#   --des 'Exp' \
-#   --itr 1 \
-#   --hidden_layers 3
-
 python -u run.py \
   --task_name long_term_forecast \
   --is_training 1 \
   --root_path ./dataset/ETT-small/ \
   --data_path ETTh1.csv \
-  --model_id ETTh1_96_192 \
+  --model_id ETTh1_96_96 \
   --model $model_name \
   --data ETTh1 \
   --features M \
   --seq_len 96 \
   --label_len 48 \
-  --pred_len 192 \
+  --pred_len 96 \
   --e_layers 2 \
   --d_layers 1 \
   --factor 3 \
@@ -44,7 +22,29 @@ python -u run.py \
   --c_out 7 \
   --des 'Exp' \
   --itr 1 \
-  --hidden_layers 5
+  --hidden_layers 3
+
+# python -u run.py \
+#   --task_name long_term_forecast \
+#   --is_training 1 \
+#   --root_path ./dataset/ETT-small/ \
+#   --data_path ETTh1.csv \
+#   --model_id ETTh1_96_192 \
+#   --model $model_name \
+#   --data ETTh1 \
+#   --features M \
+#   --seq_len 96 \
+#   --label_len 48 \
+#   --pred_len 192 \
+#   --e_layers 2 \
+#   --d_layers 1 \
+#   --factor 3 \
+#   --enc_in 7 \
+#   --dec_in 7 \
+#   --c_out 7 \
+#   --des 'Exp' \
+#   --itr 1 \
+#   --hidden_layers 5
 
 # python -u run.py \
 #   --task_name long_term_forecast \
